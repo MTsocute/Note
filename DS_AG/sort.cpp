6b3a7ac0b76e2d5f81a12b12aa9aa79718422186
MTsocute <75737534+MTsocute@users.noreply.github.com>
2024-10-23 15:42:16 +0800

    update 堆排序

--- DS_AG/sort.cpp
+++ DS_AG/sort.cpp
@@ -1,4 +1,7 @@
 #include <bits/stdc++.h>
+constexpr static int MAXHEAP_MODE = 0;
+constexpr static int MIN_HEAP_MODE = 1;
+
 
 using namespace std;
 
@@ -26,6 +29,16 @@ public:
         }
     }
 
+    template<class T>
+    void heapSort(vector<T> &arr) {
+        this->buildMaxHeap(arr);
+        for (int i = arr.size() - 1; i > 0; --i) {
+            // 动态的堆调整的范围
+            swap(arr[0], arr[i]);
+            this->heapAdjust(arr, 0, i);
+        }
+    }
+
 private:
     template<class T>
     int my_partition(vector<T> &arr, int low, int high) {
@@ -44,6 +57,35 @@ private:
         arr[low] = Pivot; // 将Pivot放回正确位置
         return low; // 返回Pivot的最终位置
     }
+
+    // 第二个参数是决定调整的范围的，非常重要
+    template<class T>
+    void heapAdjust(vector<T> & arr, int k, int heapSize) {
+        T k_val = arr[k];
+        // 如果需要调整就一直下坠
+        // 从当前节点的左孩子往下走
+        for (int left = 2 * k + 1; left < heapSize; left = 2 * left + 1) {
+            // 如果存在右孩子且右孩子比左孩子大
+            if (left + 1 < heapSize && arr[left] < arr[left + 1]) ++left;
+
+            // 分支更大结束当前分支
+            if (arr[left] <= k_val) break;
+            // 孩子比分支节点大，交换并下坠调整，
+            arr[k] = arr[left];
+            // 从节点位置哪里开始调整
+            k = left;
+        }
+        // 根节点回到原来的位置
+        arr[k] = k_val;
+    }
+
+    template<class T>
+    void buildMaxHeap(vector<T> & arr) {
+        // 从最后一个非叶子节点开始调整堆，逐步建立大根堆
+        for (int k = arr.size() / 2 - 1; k >= 0; --k)
+            this->heapAdjust(arr, k, arr.size());
+    }
+    
 };
 
 int main() {
@@ -53,7 +95,9 @@ int main() {
 
     SortLib demo;
 
-    demo.quickSort(v, 0, static_cast<int>(v.size() - 1)); // 确保使用 static_cast 转换为 int
+    // demo.quickSort(v, 0, static_cast<int>(v.size() - 1)); // 确保使用 static_cast 转换为 int
+
+    demo.heapSort(v);
 
     print_all(v);
 
